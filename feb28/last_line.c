bfe4b09e673e45aed1ede539c6a6f9dc4c53e820
gilliorem <remi.gilliot98@gmail.com>
2026-02-28 10:24:01 +0800

    writing in to my map data struct, have to fix the max width issue: not let the 'free cells' overwritten by garbage. can initialize it with 0s

---
+++ feb28/last_line.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../libft/libft.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+	int fd = open("../maps/m.cub", O_RDONLY);
+	if (fd == -1) 
+		return printf("file error\n") & 0;
+	char *line;
+	char *last_line = NULL;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break;
+		if (line[0] == '\n')
+		{
+			free(last_line);
+			last_line = line;
+			continue;
+		}
+		free(line);
+	}
+	printf("%s", line);
+	free(line);
+	free(last_line);
+}
+/*
+while (1)
+{
+	line = get_next_line(fd);
+	if (!line)
+		break;
+	if (line[0] != '\n')
+	{
+		free(line);
+		break;
+	}
+	if (last_line)
+		free(last_line);
+	last_line = ft_strdup(line);
+	free(line);
+}
+
+if (last_line)
+	printf("last line:%s", last_line);
+free(last_line);
+*/
