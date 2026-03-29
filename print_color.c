69fb73d6c3f83ff043e44ba5d4b0bf2264a623d4
gilliorem <remi.gilliot98@gmail.com>
2026-03-29 09:18:15 +0800

    feat: Notes on file parsing
    detailed explanations and pseudo code to parse the file in notes.md

--- print_color.c
+++
@@ -1,7 +0,0 @@
-void	print_color(t_color color_list[2])
-{
-	printf("%c\n", color_list[0].id);
-	printf("%i,", color_list[0].rgb[0]);
-	printf("%i,", color_list[0].rgb[1]);
-	printf("%i\n", color_list[0].rgb[2]);
-}
